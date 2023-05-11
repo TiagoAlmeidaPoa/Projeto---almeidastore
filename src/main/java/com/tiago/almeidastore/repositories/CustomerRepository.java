bf996bf878bb72176d0204c86692856805e759dd
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-05-11 19:51:44 -0300

    feat: custom validation non-repeated email on customer insertion

--- src/main/java/com/tiago/almeidastore/repositories/CustomerRepository.java
+++ src/main/java/com/tiago/almeidastore/repositories/CustomerRepository.java
@@ -2,10 +2,14 @@ package com.tiago.almeidastore.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.tiago.almeidastore.entity.Customer;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
+	@Transactional(readOnly = true)
+	Customer findByEmail(String email);
+	
 }
--- src/main/java/com/tiago/almeidastore/service/validation/CustomerInsertValidator.java
+++ src/main/java/com/tiago/almeidastore/service/validation/CustomerInsertValidator.java
@@ -6,27 +6,42 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.tiago.almeidastore.controller.exception.FieldMessage;
 import com.tiago.almeidastore.dto.CustomerNewDTO;
+import com.tiago.almeidastore.entity.Customer;
 import com.tiago.almeidastore.entity.enums.TypeCustomer;
+import com.tiago.almeidastore.repositories.CustomerRepository;
 import com.tiago.almeidastore.service.validation.utils.BR;
 
 public class CustomerInsertValidator implements ConstraintValidator<CustomerInsert, CustomerNewDTO> {
+	
 	@Override
 	public void initialize(CustomerInsert ann) {
 	}
+	
+	@Autowired
+	private CustomerRepository repository;
 
 	@Override
 	public boolean isValid(CustomerNewDTO objDto, ConstraintValidatorContext context) {
-		
+
 		List<FieldMessage> list = new ArrayList<>();
-		
-		if(objDto.getTypeCustomer().equals(TypeCustomer.NATURALPERSON.getCode()) && !BR.isValidCPF(objDto.getCpfOrCnpj())) {
+
+		if (objDto.getTypeCustomer().equals(TypeCustomer.NATURALPERSON.getCode())
+				&& !BR.isValidCPF(objDto.getCpfOrCnpj())) {
 			list.add(new FieldMessage("cpfOrCnpj", "Invalid CPF"));
 		}
-		
-		if(objDto.getTypeCustomer().equals(TypeCustomer.LEGALPERSON.getCode()) && !BR.isValidCNPJ(objDto.getCpfOrCnpj())) {
+
+		if (objDto.getTypeCustomer().equals(TypeCustomer.LEGALPERSON.getCode())
+				&& !BR.isValidCNPJ(objDto.getCpfOrCnpj())) {
 			list.add(new FieldMessage("cpfOrCnpj", "Invalid CNPJ"));
+		}
+		
+		Customer aux = repository.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email","Email already exists"));
 		}
 
 		// inclua os testes aqui, inserindo erros na lista
